1b1f7c608acd5a02ae5143e7702d8b335b38d2d5
rorysg <45520790+rorysg@users.noreply.github.com>
2018-12-23 14:17:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.getElementById("myBtn").addEventListener("click", addItem);
 
 function addItem() {
-	let item = document.getElementById("field").value;
-	document.getElementById("print").appendChild(document.createTextNode(item));
+	const item = document.getElementById("field").value;
+	let ul = document.getElementById("list");
+	let li = document.createElement("li");
+
+	li.appendChild(document.createTextNode(item));
+	document.getElementById("list").appendChild(li);
+
+	document.getElementById("field").value = "";
 }
